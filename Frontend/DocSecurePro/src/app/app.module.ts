a1b9606b05b312a35fb630d9abc538e53df98015
Alvaro Rodriguez Ruiz <itzalvi03@gmail.com>
2023-12-23 13:49:58 +0100

    Pantalla login hecha

--- Frontend/DocSecurePro/src/app/app.module.ts
+++ Frontend/DocSecurePro/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PantallaLoginComponent } from './components/pantalla-login/pantalla-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PantallaLoginComponent
   ],
   imports: [
     BrowserModule,
