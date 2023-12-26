da5a1b90dca9dd57fbc667f962cdaf3ca0e79c87
Alberth <alciviarymarck@gmail.com>
2023-12-26 13:17:00 +0100

    Index inicio

--- Frontend/DocSecurePro/src/app/app.module.ts
+++ Frontend/DocSecurePro/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { IndexComponent } from './index/index.component';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    MdbFormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
