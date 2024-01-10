659cb47e7b6e014aa2c15d739c81b92aab5ab426
Aida Tejada <aidatejadamarin@gmail.com>
2024-01-10 19:54:05 +0100

    navbar estilos y su routing

--- Frontend/DocSecurePro/src/app/app-routing.module.ts
+++ Frontend/DocSecurePro/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IndexComponent } from './index/index.component';
+import { IndexComponent } from './components/index/index.component';
 import { ArchivosComponent } from './components/archivos/archivos.component';
+import { PantallaLoginComponent } from './components/pantalla-login/pantalla-login.component';
 
 const routes: Routes = [
-  { path: '', component: IndexComponent },
-  { path: 'archivos', component: ArchivosComponent }
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirigir de la ruta vacía a /home
+  { path: 'home', component: IndexComponent },
+  { path: 'archivos', component: ArchivosComponent },
+  { path: 'logIn', component:PantallaLoginComponent}
 ];
 
 @NgModule({
